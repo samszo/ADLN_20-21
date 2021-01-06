993f3737436cf50b795a4e108129617737708740
PhilippeP8 <72509273+PhilippeP8@users.noreply.github.com>
2021-01-06 09:56:40 +0100

    Update main.js

--- dessetrousse/main.js
+++ dessetrousse/main.js
@@ -7,7 +7,7 @@
 	let gen = genF(21);
 	gen.faire(["mourir","Marquise"]);
 	gen.faire(["#"]);
-	console.log (gen.finie);
+	console.log (gen.finie());
 	gen.faire(["deplacer","Dessetrousse","rue"]);
-	console.log (gen.finie);
+	console.log (gen.finie());
 	console.log(gen.story);
