1cf286be621f38410b0a8f70720df05db0c6314c
Philipp Khon <47764205+shareBND@users.noreply.github.com>
2021-01-06 14:52:30 +0100

    Add files via upload

--- projet1/main.js
+++ projet1/main.js
@@ -1,12 +1,4 @@
-	/* 
-		remplacez le chiffre 21 ci-dessous par l'ID de votre vocabulaire qui est, selon le groupe : 
-		gr 1 : 16
-		gr 2 : 7
-		gr 3 : 6
-	*/
-	let gen = genF(16);
-	console.log(gen);
-	console.log(gen.Monde[0].values[7].Description);
-	//document.getElementById("Line1").innerHTML=gen.Monde[0].values[7].Description;
-
+let gen = genF(16);
+console.log(gen);
+console.log(gen.Monde[0].values[7].Description);
 
