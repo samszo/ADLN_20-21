deb9943c60f5b4521d91afc96533f81dc4ef4b99
PhilippeP8 <72509273+PhilippeP8@users.noreply.github.com>
2021-01-07 11:19:20 +0100

    Update main.js

--- dessetrousse/main.js
+++ dessetrousse/main.js
@@ -5,9 +5,40 @@
 		gr 3 : 6
 	*/
 	let gen = genF(21);
-	gen.faire(["mourir","Marquise"]);
-	gen.faire(["#"]);
-	console.log (gen.finie());
-	gen.faire(["deplacer","Dessetrousse","rue"]);
-	console.log (gen.finie());
-	console.log(gen.story);
+
+
+// début de l'histoire
+// on exécute des évènements peut conséquents
+
+	let pRue;
+	let pMalade;
+	let liste;
+	let Marquise;
+	for (let i = 0; i<4 ; i++) {
+		pRue = (i+1)*3;
+		pMalade = Math.max(i,2)*3;
+		liste = [
+			[["deplacer","flic","rue"],3],
+			[["deplacer","Dessetrousse","rue"],pRue],
+			[["deplacer","Marquise","salon"],2],
+			[["deplacer","Marquise","chambre"],3],
+			[["malade","Marquise"],3],
+			[["malade","Dessetrousse"],pMalade],
+			[["deplacer","Fils","rue"],2],
+			[["mourirCardiaque","Marquise"],1]			
+		];
+		gen.faireAvecProba(liste);
+		Marquise = gen.getItem("Personnage","nom","Marquise");
+		if (!Marquise["en vie"]) {gen.faire(["*"]);}
+	}
+	
+	nonFin();
+	
+function nonFin (){
+	let retour = gen.finie();
+	if (retour){
+		document.querySelector("#story").innerHTML = gen.copieTable(gen.story);
+	}
+}
+
+
