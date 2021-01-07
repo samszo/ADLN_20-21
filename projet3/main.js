8d5dd7dd43d54d0d677a98292763335e4a21e7b5
PhilippeP8 <72509273+PhilippeP8@users.noreply.github.com>
2021-01-07 22:30:02 +0100

    Update main.js

--- projet3/main.js
+++ projet3/main.js
@@ -5,8 +5,12 @@
 		gr 3 : 6
 	*/
 	let gen = genF(6);
-// les 2 lignes suivantes corrigent une erreur dans le traitement des données omeka et sont donc nécessaires
+// les 6 lignes suivantes corrigent une erreur dans le traitement des données omeka et sont donc nécessaires
 		gen.getItem("Event","function","kill").tablesParameters = ["Character","Character"];
 		gen.getItem("Event","function","attack").tablesParameters = ["Character","Character","Location"];
+		console.group();
+		console.log(gen.copieTable(gen.Monde)); // affichage console de la table du monde corrigée
+		console.log(gen.copieTable(gen.Event)); // affichage console de la table des évènements corrigée
+		console.groupEnd();
 		
 // votre programme débute ici
