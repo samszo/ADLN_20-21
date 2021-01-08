77555905c264c0967caa211256e9093d30a18258
PhilippeP8 <72509273+PhilippeP8@users.noreply.github.com>
2021-01-08 10:45:58 +0100

    Update main.js

--- projet3/main.js
+++ projet3/main.js
@@ -5,10 +5,11 @@
 		gr 3 : 6
 	*/
 	let gen = genF(6);
-// les lignes suivantes corrigent une erreur dans le traitement des données omeka et sont donc nécessaires
-		gen.getItem("Event","function","kill").tablesParameters = ["Character","Character"];
-		gen.getItem("Event","function","attack").tablesParameters = ["Character","Character","Location"];
-		console.group("affichage des tables corrigées : table du Monde puis table des évènements");
+// les lignes suivantes corrigent une erreur dans le traitement des données omeka de tablesParameters et sont donc nécessaires
+		gen.getItem("Event","function","kill").tablesParameters = ["Character","Character"]; // item 180
+		gen.getItem("Event","function","attack").tablesParameters = ["Character","Character","Location"]; // item 
+		console.group("affichage des tables corrigées : table du Monde puis table des évènements"); // item 182
+		// curieusement pas de pb pour l'item 188 pour lequel le doublon se trouve sur les 2 dernières lignes du champ
 		console.log(gen.copieTable(gen.Monde)); // affichage console de la table du monde corrigée
 		console.log(gen.copieTable(gen.Event)); // affichage console de la table des évènements corrigée
 		console.groupEnd();
